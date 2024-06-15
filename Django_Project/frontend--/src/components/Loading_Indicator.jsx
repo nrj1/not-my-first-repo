d3d5a7c3c7c4a8f7621be2654f0851303f566925
the_da_vinci_coder <neilrajeevjohn@gmail.com>
2024-06-15 15:41:14 -0400

    first-commit

---
+++ Django_Project/frontend--/src/components/Loading_Indicator.jsx
@@ -0,0 +1,9 @@
+import "../styles/Loading_Indicator.css"
+
+const LoadingIndicator = () => {
+    return <div className="loading-container">
+        <div className="loader"></div>
+    </div>
+}
+
+export default LoadingIndicator
