d33674f86c227f810be1667df1f1878772109b80
the_da_vinci_coder <neilrajeevjohn@gmail.com>
2024-06-16 12:16:56 -0400

    Update api.js
    Forgot this last time, coz I had made changes to a file in a different directory rather than this one (copies of each other)

--- Django_Project/frontend--/src/api.js
+++ Django_Project/frontend--/src/api.js
@@ -4,7 +4,7 @@
  const apiUrl = "https://50368a2d-4b87-41a3-ac5e-fd58befe3e2d-dev.e1-us-east-azure.choreoapis.dev/django-react-project/backend/v1"
 
  const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
  })
 
  api.interceptors.request.use(
