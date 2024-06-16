3a43748e8238b7bca61f96a214f5822a8da56f70
the_da_vinci_coder <neilrajeevjohn@gmail.com>
2024-06-16 12:05:02 -0400

    Update api.js

--- Django_Project/frontend--/src/api.js
+++ Django_Project/frontend--/src/api.js
@@ -1,6 +1,8 @@
  import axios from "axios"
  import { ACCESS_TOKEN } from "./constants"
 
+ const apiUrl = "https://50368a2d-4b87-41a3-ac5e-fd58befe3e2d-dev.e1-us-east-azure.choreoapis.dev/django-react-project/backend/v1"
+
  const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
  })
